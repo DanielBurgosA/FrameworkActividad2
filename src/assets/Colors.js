3f909e9bd6115d3dfaaaa378d27f62109e591365
Daniel Burgos <dburgos.developer@gmail.com>
2024-03-13 23:32:17 -0500

    first commit

---
+++ src/assets/Colors.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+const Colors = {
+    black: "#2D3436",
+    gray: "#A4A4A4",
+    lightGray: "#CACACA",
+    blue: "#24A6D9",
+    ligthBlue: "#A7CBD9",
+    white: "#FFFFFF"
+}
+
+export default Colors
